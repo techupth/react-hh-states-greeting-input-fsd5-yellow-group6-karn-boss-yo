b368b405f7ca925da1585c1f4a97686f7064c3fd
noppadon <noppadonsangthongw@gmail.com>
2023-12-20 19:20:09 +0700

    add: input text

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [valueString, setValueString] = useState('Greeting Message')
+
+  const changeStringNew = () => {
+    const newGreetingMessage = document.getElementById('greeting-message').value;
+    setValueString(newGreetingMessage);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{valueString}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
         <input id="greeting-message" type="text" />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={changeStringNew}>Update text</button>
       </div>
     </div>
   );
