d19c41ac6f083b1ac205c9e75cda7f3afec33ca9
BOSS <bigboss1796@hotmail.com>
2023-12-21 13:57:09 +0700

    feat:React State ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("Greeting Message");
+
+  const updateText = () => {
+    const newText = document.getElementById("greeting-message").value;
+    setText(newText)
+  }
+
+  //document.getElentById("").value คือการดึงค่าที่ผู้ใช้ป้อนเข้าใน Element ที่มี ID ที่ระบุ ใน DOM (Document Object Model) ของเว็บไซต์ของคุณ.ณ ที่นี้คือ id-"greeting-message" ของ <input>
+  //นามสกุล .value จะถูกใช้เพื่อดึงค่าที่ผู้ใช้ป้อนลงใน input element
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{text}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
         <input id="greeting-message" type="text" />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateText}>Update text</button>
       </div>
     </div>
   );
